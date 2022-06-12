c3465693cbd5894b616e2c54b2e3e994e9256c4c
Zero Sugar Ashton <treixatek@gmail.com>
2022-06-12 12:11:56 -0400

    Update Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -1,1 +1,5 @@
 //this is just a random file
+UE_LOG(LogShaders, Fatal, TEXT("Failed to compile global shader %s %s %s.  Enable 'r.ShaderDevelopmentMode' in ConsoleVariables.ini for retries."),
+			GlobalShaderType->GetName(),
+			Pipeline ? TEXT("for pipeline") : TEXT(""),
+			Pipeline ? Pipeline->GetName() : TEXT(""));
